03205abecde54e7b870a2434b378bc1dbdd1d97a
krishnakumar262006-maker <krishnakumar262006@gmail.com>
2025-11-12 20:54:27 +0530

    Fix server.js to correct server running URL

--- server.js
+++ server.js
@@ -66,4 +66,5 @@ mongoose.connect(MONGO)
 
 // Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}/home.html`));
+
